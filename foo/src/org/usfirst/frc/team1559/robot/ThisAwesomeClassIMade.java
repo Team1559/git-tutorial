d80a7b39f87086a7485826af42cc230ed1b76edd
Cody Burrows <code1010@rochester.rr.com>
2015-12-16 15:59:43 -0500

    I am being productive
    This code doesn't work yet

---
+++ foo/src/org/usfirst/frc/team1559/robot/ThisAwesomeClassIMade.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team1559.robot;
+
+public class ThisAwesomeClassIMade {
+
+	private int foo;
+	
+	public ThisAwesomeClassIMade(){
+		foo = 0;
+	}
+	
+	public int getFoo(){
+		return foo;
+	}
+	
+	public void setFoo(int f){
+		foo = f;
+	}
+	
+	foo foo foo foofoo
+	this class won't compile now lol'
+	
+}
